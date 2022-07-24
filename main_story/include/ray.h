e0b2ecfec0dbf5c71f80f2733d54159c74e17bba
aki <aki@mymail.com>
2022-07-25 01:35:43 +0900

    for explain ray as line half-line vector

---
+++ main_story/include/ray.h
@@ -0,0 +1,37 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include<vec3.h>
+
+class ray{
+    public:
+
+	ray(void) {}
+
+	ray(const point3& origin, const vec3& direction)
+	    : orig(origin),dir(direction)
+	{}
+
+	pouint3 origin(void) const {
+
+	    return orig;
+	}
+
+	vec3 direction(void) const{
+
+	    return dir;
+	}
+
+	point3 at(double t) const{
+
+	    return orig + t*dir;
+	}
+
+    public:
+
+	point3 orig;
+	vec3   dir ;
+};
+
+
+#endif
