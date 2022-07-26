e72b71b5876f01a38a367f5f1778c8b50490923c
aki <aki@mymail.com>
2022-07-27 00:00:53 +0900

    配列の型をテンプレートを使うとうまくいかない

--- Appendix/ostream/array.cpp
+++ Appendix/ostream/array.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-namespace array_3{
+namespace array_0{
 
     struct array_3{
 
@@ -20,11 +20,34 @@ namespace array_3{
 
 }
 
+namespace array_1{
+
+    template<typename data_type>
+	struct array_3{
+
+	    data_type data[3];
+
+	    data_type& operator [](std::size_t i){
+
+		return data[i];
+	    }
+	};
+
+    template<typename array_3>
+    std::ostream& operator << (std::ostream& os,array_3 array){
+
+	return os << array[0] <<' '<< array[1] 
+	    << ' ' << array[2] << "\n";
+    }
+}
+
 int main(void){
 
-    array_3::array_3 a = {1,2,3};
-
+    array_0::array_3 a = {1,2,3};
     std::cout << a;
+
+    array_1::array_3<int> b = {2,4,6};
+    std::cout << b;
 
 
     return 0;
